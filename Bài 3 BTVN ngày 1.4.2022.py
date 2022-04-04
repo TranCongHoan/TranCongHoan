cc33d2e00dfffc386747c2fa3bc2f4002f992c40
Trần Công Hoan <hoanhaui1993@gmail.com>
2022-04-04 22:17:04 +0700

    Update Bài 3 BTVN ngày 1.4.2022.py

--- Bài 3 BTVN ngày 1.4.2022.py
+++ Bài 3 BTVN ngày 1.4.2022.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -34,3 +35,41 @@ else :
 
 
 
+=======
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a = int(input('nhập số bất kì: '))
+if a == 0:
+    print ('hôm nay là thứ 2')
+elif a == 1:
+    print ('hôm nay là thứ 3')
+elif a == 2:
+    print ('hôm nay là thứ 4')
+elif a == 3:
+    print ('hôm nay là thứ 5')
+elif a == 4:
+    print ('hôm nay là thứ 6')
+elif a == 5:
+    print ('hôm nay là thứ 7')
+elif a == 6:
+    print ('hôm nay là chủ nhật')
+else :
+    print ('nhập sai dữ liệu, mời nhập lại')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+>>>>>>> 7a243eb0a4426bfd9ec9af9b6ee942b42af7aa0d
